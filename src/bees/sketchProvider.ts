c027b0cde4f615a0d0f626297e9bd0d4b7dbb66d
Monarch Wadia <monarchwadia@gmail.com>
2024-02-22 20:28:06 -0500

    Colors. Also, added hive.

--- src/bees/sketchProvider.ts
+++ src/bees/sketchProvider.ts
@@ -9,9 +9,19 @@ export const sketchProvider = (state: WorldState) => (p: p5) => {
   p.draw = () => {
     p.background(0);
 
+    // paint the hive in the center
+    p.fill([185, 125, 0]);
+    p.ellipse(state.config.width / 2, state.config.height / 2, 30, 30);
+
     // paint the bees
     state.objects.forEach((obj) => {
       switch (obj.type) {
+        case "trail-point":
+          // trail strength is from 1 to 100
+          const opacity = Math.floor((obj.strength / 100) * 255);
+          p.fill([185, 100, 100, opacity]);
+          p.ellipse(obj.x, obj.y, 5);
+          break;
         case "bee":
           p.fill([255, 255, 0]);
           p.rect(obj.x, obj.y, 10, 10);
@@ -19,12 +29,6 @@ export const sketchProvider = (state: WorldState) => (p: p5) => {
         case "flower":
           p.fill([255, 125, 125]);
           p.ellipse(obj.x, obj.y, 20, 20);
-          break;
-        case "trail-point":
-          // trail strength is from 1 to 100
-          const color = Math.floor((obj.strength / 100) * 255);
-          p.fill([color, color, color]);
-          p.ellipse(obj.x, obj.y, 5);
           break;
       }
     });
