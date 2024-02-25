0109b7446a0636714e6aabf4715ba6bd55bc7e16
Monarch Wadia <monarchwadia@gmail.com>
2024-02-24 19:44:27 -0500

    Bee cosntants

--- src/bees/constants.ts
+++ src/bees/constants.ts
@@ -9,6 +9,6 @@ export const BEE_OBJ_INTERACTION_RANGE = 10; // how close the bee needs to be to
 export const BEE_TRAIL_POINT_POWER = 2; // how much the trail point influences the bee's movement
 export const BEE_ROTATION_SPEED_MIN = 0.004;
 export const BEE_ROTATION_SPEED_MAX = 0.008;
-export const BEE_TRAIL_POINT_DROP_CHANCE = 0.02;
+export const BEE_TRAIL_POINT_DROP_CHANCE = 0.2;
 export const BEE_HUNGER_DEATH_THRESHOLD = 100;
 export const BEE_HUNGER_FEEDING_THRESHOLD = 50;
